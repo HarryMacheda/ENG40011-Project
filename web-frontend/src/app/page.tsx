b1b91da86bed758ab27a361aa4086265cac15b76
Harry Macheda <harrymacheda@outlook.com>
2025-09-26 11:27:34 +1000

    teardrop component

--- web-frontend/src/app/page.tsx
+++ web-frontend/src/app/page.tsx
@@ -1,9 +1,26 @@
-
+"use client"
+import Teardrop from "@/components/teardrop";
+import React, { useState } from "react";
+import { Box, Slider, Typography } from "@mui/material";
 
 export default function Home() {
+  const [value, setValue] = useState(50);
+
   return (
-    <div>
-        Hello world
-    </div>
+    <Box display="flex" flexDirection="column" alignItems="center" gap={4}>
+      <Teardrop value={value} size={500} />
+
+      <Box width={300}>
+        <Typography gutterBottom>Fill Level</Typography>
+        <Slider
+          value={value}
+          onChange={(e, newValue) => setValue(newValue)}
+          step={1}
+          min={0}
+          max={100}
+          valueLabelDisplay="auto"
+        />
+      </Box>
+    </Box>
   );
 }
