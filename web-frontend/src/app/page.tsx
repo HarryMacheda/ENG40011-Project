86be9860b7fcfa12003b410b2df7fe905d02f09b
Harry Macheda <harrymacheda@outlook.com>
2025-09-26 13:22:42 +1000

    added alert when clood is detected

--- web-frontend/src/app/page.tsx
+++ web-frontend/src/app/page.tsx
@@ -1,9 +1,10 @@
 "use client"
 import React from "react";
-import { Box, ThemeProvider } from "@mui/material";
+import { Box, Button, ThemeProvider } from "@mui/material";
 import { AuthProvider } from "@/contexts/auth";
 import theme from "@/theme";
 import TeardropView from "./views/TeardropView";
+import { AlertsProvider } from "@/contexts/alerts";
 
 
 export default function Home() {
@@ -12,9 +13,11 @@ export default function Home() {
   return (
     <ThemeProvider theme={theme}>
       <AuthProvider>
-        <Box display="flex" flexDirection="column" alignItems="center" gap={4}>
-          <TeardropView/>
-        </Box>
+        <AlertsProvider>
+          <Box display="flex" flexDirection="column" alignItems="center" gap={4}>
+            <TeardropView/>
+          </Box>
+        </AlertsProvider>
       </AuthProvider>
     </ThemeProvider>
   );
--- web-frontend/src/app/views/TeardropView.tsx
+++ web-frontend/src/app/views/TeardropView.tsx
@@ -1,17 +1,25 @@
 import { ApiClient } from "@/utility/api-client";
 import { useWebSocket } from "@/hooks/useWebSockets";
-import { useMemo } from "react";
+import { useEffect, useMemo } from "react";
 import { useAuth } from "@/contexts/auth";
 import Teardrop from "@/components/teardrop";
 import { Typography } from "@mui/material";
 import { ColourToHex } from "@/utility/colour";
+import { useAlerts } from "@/contexts/alerts";
 
 
 
 export default function TeardropView() {
     const {token} = useAuth();
     const apiClient = useMemo(() => new ApiClient({baseUrl:"http://127.0.0.1:8000"}), [token]);
-    const {isConnected, lastMessage} = useWebSocket<any>(apiClient, `/liquid/colour/subscribe?token=${token}`, );
+    const {lastMessage} = useWebSocket(apiClient, `/liquid/colour/subscribe?token=${token}`, );
+    const { showAlert } = useAlerts();
+
+    useEffect(() => {
+        if(lastMessage && lastMessage.isBlood) {
+           showAlert("Blood detected!", "error")
+        }
+    }, lastMessage)
 
     return (<>
         <Teardrop value={50} size={500} colour={lastMessage ? ColourToHex(lastMessage) : undefined} />  
